170d1fb0e81a194355fc150dfe79d162ecf89ece
timofruehwirth <xml.codemonkey@gmail.com>
2022-02-28 11:42:33 +0100

    create `\ric_metadata\ric_metadata.py`

---
+++ ric_metadata/data/py/ric_metadata.py
@@ -0,0 +1,27 @@
+# import csv module
+import csv
+
+# create `ric_metadata.xml`
+xml_file = open("ric_metadata.xml", "w")
+
+# opening file
+with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.csv", "r", newline='') as csv_file:
+    table = csv.DictReader(csv_file, delimiter=',', quotechar='"')
+    for row in table:
+        doc_id = row['document_id']
+        doc_title = row['document_title']
+        doc_date = row['document_date']
+        doc_author = row['document_author']
+        xml_file.write("\n<xml doc type etc.>")
+        xml_file.write("\n<further xml info etc.>")
+        if doc_id != "":
+            xml_file.write("\n<" + doc_id + ">")
+        if doc_title != "":
+            xml_file.write("\n<" + doc_title + ">")
+        if doc_date != "":
+            xml_file.write("\n<" + doc_date + ">")
+        if doc_author != "":
+            xml_file.write("\n<" + doc_author + ">")
+        xml_file.write("\n")
+
+xml_file.close()
