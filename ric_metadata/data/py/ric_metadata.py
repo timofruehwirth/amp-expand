140ccdf65ffd8fae0c1510f1e5d38e0b6485ecec
timofruehwirth <xml.codemonkey@gmail.com>
2022-03-09 16:39:39 +0100

    add RiC agent, add record URIs

--- ric_metadata/data/py/ric_metadata.py
+++ ric_metadata/data/py/ric_metadata.py
@@ -2,14 +2,22 @@
 # import csv module
 import csv
 
-# create `ric_metadata.xml`
-xml_file = open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.xml", "w")
+# create rdf/xml file
+rdf_file = open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.rdf", "w")
 
 # write xml declaration and root element
-xml_file.write("<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- in development! -->")
-xml_file.write("\n<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"")
-xml_file.write("\n         xmlns:rico=\"https://www.ica.org/standards/RiC/ontology#\">")
-xml_file.write("\n")
+rdf_file.write("<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- this file is in development! -->")
+rdf_file.write("\n<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"")
+rdf_file.write("\n         xmlns:rico=\"https://www.ica.org/standards/RiC/ontology#\">")
+rdf_file.write("\n")
+rdf_file.write("\n<!-- ric agents -->")
+rdf_file.write("\n<rico:Agent rdf:about=\"https://amp.acdh.oeaw.ac.at/whauden.html\">")
+rdf_file.write("\n   <rico:hasOrHadAgentName>")
+rdf_file.write("\n      Auden, W. H.")
+rdf_file.write("\n   </rico:hasOrHadAgentName>")
+rdf_file.write("\n</rico:Agent>")
+rdf_file.write("\n")
+rdf_file.write("\n<!-- ric records -->")
 
 # open `ric.metadata.csv`
 with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.csv", "r", newline='') as csv_file:
@@ -23,23 +31,24 @@ with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.
         doc_author = row['document_author']
         doc_language = row['document_language']
         # write variables in new file (unless empty)
-        xml_file.write("\n<rico:Record>")
+        rdf_file.write("\n<rico:Record rdf:about=\"https://amp.acdh.oeaw.ac.at/" + doc_id + ".html\">")
         if doc_id != "":
-            xml_file.write("\n   <rico:isOrWasIdentifierOf>amp_" + doc_id + "</rico:isOrWasIdentifierOf>")
+            rdf_file.write("\n   <rico:hasOrHadIdentifier>amp_" + doc_id + "</rico:hasOrHadIdentifier>")
         if doc_title != "":
-            xml_file.write("\n   <rico:hasOrHadName>" + doc_title + "</rico:hasOrHadName>")
+            rdf_file.write("\n   <rico:hasOrHadName>" + doc_title + "</rico:hasOrHadName>")
         if doc_date != "":
-            xml_file.write("\n   <rico:isAssociatedWithDate>" + doc_date + "</rico:isAssociatedWithDate>")
+            rdf_file.write("\n   <rico:isAssociatedWithDate>" + doc_date + "</rico:isAssociatedWithDate>")
         if doc_author != "":
-            # replace with URL identifier
-            xml_file.write("\n   <rico:hasAuthor>" + doc_author + "</rico:hasAuthor>")
+            # add RiC agents
+            if doc_author == "Auden, W. H.":
+                rdf_file.write("\n   <rico:hasAuthor>https://amp.acdh.oeaw.ac.at/whauden.html</rico:hasAuthor>")
         if doc_language != "":
-            xml_file.write("\n   <rico:hasOrHadLanguage>" + doc_language + "</rico:hasOrHadLanguage>")
-        xml_file.write("\n</rico:Record>")
-        xml_file.write("\n")
+            rdf_file.write("\n   <rico:hasOrHadLanguage>" + doc_language + "</rico:hasOrHadLanguage>")
+        rdf_file.write("\n</rico:Record>")
+        rdf_file.write("\n")
 
 # close xml root element
-xml_file.write("\n</rdf:RDF>")
+rdf_file.write("\n</rdf:RDF>")
 
-# close `ric_metadata.xml`
-xml_file.close()
+# close rdf/xml file
+rdf_file.close()
