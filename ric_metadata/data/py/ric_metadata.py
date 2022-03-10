049bcb6bb279abc1802ded4ddab54a6880352430
timofruehwirth <xml.codemonkey@gmail.com>
2022-03-10 12:33:57 +0100

    add `partition` function for `doc_author_abbr`

--- ric_metadata/data/py/ric_metadata.py
+++ ric_metadata/data/py/ric_metadata.py
@@ -10,8 +10,9 @@ rdf_file.write("<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- this file is in d
 rdf_file.write("\n<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"")
 rdf_file.write("\n         xmlns:rico=\"https://www.ica.org/standards/RiC/ontology#\">")
 rdf_file.write("\n")
+# write ric agent entities
 rdf_file.write("\n<!-- ric agents -->")
-rdf_file.write("\n<rico:Agent rdf:about=\"https://amp.acdh.oeaw.ac.at/whauden.html\">")
+rdf_file.write("\n<rico:Agent rdf:about=\"https://amp.acdh.oeaw.ac.at/auden.html\">")
 rdf_file.write("\n   <rico:hasOrHadAgentName>")
 rdf_file.write("\n      Auden, W. H.")
 rdf_file.write("\n   </rico:hasOrHadAgentName>")
@@ -40,8 +41,8 @@ with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.
             rdf_file.write("\n   <rico:isAssociatedWithDate>" + doc_date + "</rico:isAssociatedWithDate>")
         if doc_author != "":
             # add RiC agents
-            if doc_author == "Auden, W. H.":
-                rdf_file.write("\n   <rico:hasAuthor>https://amp.acdh.oeaw.ac.at/whauden.html</rico:hasAuthor>")
+            doc_author_abbr = doc_author.partition(',')
+            rdf_file.write("\n   <rico:hasAuthor>https://amp.acdh.oeaw.ac.at/" + doc_author_abbr[0].lower() + ".html</rico:hasAuthor>")
         if doc_language != "":
             rdf_file.write("\n   <rico:hasOrHadLanguage>" + doc_language + "</rico:hasOrHadLanguage>")
         rdf_file.write("\n</rico:Record>")
