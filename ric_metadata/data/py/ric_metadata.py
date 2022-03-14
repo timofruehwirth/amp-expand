7a54f6f331dc930f3c787ea1a59414abd8091af6
timofruehwirth <xml.codemonkey@gmail.com>
2022-03-14 16:49:23 +0100

    add `rico:hasContentOfType`

--- ric_metadata/data/py/ric_metadata.py
+++ ric_metadata/data/py/ric_metadata.py
@@ -1,9 +1,11 @@
-# in development!
+# this file is in development!
 # import csv module
 import csv
 
-# create list of ric agents
+# create list of ric agents, record and agent numerators
 doc_agent_list = []
+number_records = 0
+number_agents = 0
 
 # create rdf/xml file
 rdf_file = open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.rdf", "w")
@@ -29,6 +31,7 @@ with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.
         doc_date = row['document_date']
         doc_author = row['document_author']
         doc_language = row['document_language']
+        doc_content_type = row['content_type']
         # insert variables (unless empty) into ric-record entry in rdf/xml file
         rdf_file.write("\n<rico:Record rdf:about=\"https://amp.acdh.oeaw.ac.at/amp-transcript__" + doc_id + ".html\">")
         if doc_id != "":
@@ -40,10 +43,13 @@ with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.
         if doc_author != "":
             doc_author_abbr = doc_author.partition(',')
             rdf_file.write("\n   <rico:hasAuthor>https://amp.acdh.oeaw.ac.at/" + doc_author_abbr[0].lower() + ".html</rico:hasAuthor>")
+        if doc_content_type != "":
+            rdf_file.write("\n   <rico:hasContentOfType>" + doc_content_type + "</rico:hasContentOfType>")
         if doc_language != "":
             rdf_file.write("\n   <rico:hasOrHadLanguage>" + doc_language + "</rico:hasOrHadLanguage>")
         rdf_file.write("\n</rico:Record>")
         rdf_file.write("\n")
+        number_records += 1
 
 # introduce ric-agents section
 rdf_file.write("\n<!-- ric agents -->")
@@ -68,9 +74,16 @@ with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.
             rdf_file.write("\n")
             # add unlisted agent to agents list
             doc_agent_list.append(doc_agent)
+            number_agents += 1
 
 # close rdf/xml root element
 rdf_file.write("\n</rdf:RDF>")
 
 # close rdf/xml file
 rdf_file.close()
+
+# print log
+print("")
+print(str(number_records) + " record entry/ies written; " + str(number_agents) + " agent entry/ies written.\n")
+print("             __|__\n        ______(_)______\n            \"  \"  \"\n")
+print("Thank you for flying with us.")
