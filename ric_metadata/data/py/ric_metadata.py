7b0199cf90922b1992ec2849286806ec6cd3d6e5
timofruehwirth <xml.codemonkey@gmail.com>
2022-04-05 18:16:54 +0200

    add lxml library to ric_metadata.py

--- ric_metadata/data/py/ric_metadata.py
+++ ric_metadata/data/py/ric_metadata.py
@@ -3,57 +3,63 @@
 # import csv module
 import csv
 
-# import datetime class from datetime module
+# import datetime.datetime module
 from datetime import datetime
 
-# create list of ric agents, record and agent numerators
+# this script uses the lxml library for creating xml
+# alternatively the rdflib library for creating rdf might be used
+
+# import lxml.etree library
+from lxml import etree
+
+# define rdf and rico namespace prefixes
+nsmap = {"rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#", "rico": "https://www.ica.org/standards/RiC/ontology#"}
+
+# create rdf/xml root element
+root = etree.Element(etree.QName(nsmap["rdf"], "RDF"), nsmap=nsmap)
+
+# create list of ric agents, create ric record and agent numerators
 doc_agent_list = []
 number_records = 0
 number_agents = 0
 
-# create rdf/xml file
-rdf_file = open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.rdf", "w")
-
-# write xml declaration and rdf/xml root element
-rdf_file.write("<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- this file is in development! -->")
-rdf_file.write("\n<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"")
-rdf_file.write("\n         xmlns:rico=\"https://www.ica.org/standards/RiC/ontology#\">")
-rdf_file.write("\n")
-
-# introduce ric-records section
-rdf_file.write("\n<!-- ric records -->")
-rdf_file.write("\n")
-
-# open `ric.metadata.csv`
+# open `ric_metadata.csv`
 with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.csv", "r", newline='') as csv_file:
-    # read file and map data in dictionary
+    # read csv file and map data in dictionary
     table = csv.DictReader(csv_file, delimiter=',', quotechar='"')
     for row in table:
         # store cell data in variables
         doc_id = row['document_id']
         doc_title = row['document_title']
         doc_date = row['document_date']
-        doc_author = row['document_author']
+        doc_author_01 = row['document_author_01']
+        doc_author_02 = row['document_author_02']
         doc_language = row['document_language']
         doc_content_type = row['content_type']
-        # insert variables (unless empty) into ric-record entry in rdf/xml file
-        rdf_file.write("\n<rico:Record rdf:about=\"https://amp.acdh.oeaw.ac.at/amp-transcript__" + doc_id + ".html\">")
+        # create rdf/xml record element and sub-elements
+        record = etree.SubElement(root, etree.QName(nsmap['rico'], 'Record'))
+        record.attrib[etree.QName(nsmap["rdf"], "about")] = "https://amp.acdh.oeaw.ac.at/amp-transcript__" + doc_id
+        # insert variables unless empty
         if doc_id != "":
-            rdf_file.write("\n   <rico:hasOrHadIdentifier>amp_" + doc_id + "</rico:hasOrHadIdentifier>")
+            etree.SubElement(record, etree.QName(nsmap["rico"], "hasOrHadIdentifier")).text = "amp_" + doc_id
         if doc_title != "":
-            rdf_file.write("\n   <rico:hasOrHadName>" + doc_title + "</rico:hasOrHadName>")
+            etree.SubElement(record, etree.QName(nsmap["rico"], "hasOrHadName")).text = doc_title
         if doc_date != "":
-            rdf_file.write("\n   <rico:isAssociatedWithDate>" + doc_date + "</rico:isAssociatedWithDate>")
-        if doc_author != "":
-            doc_author_abbr = doc_author.partition(',')
-            rdf_file.write("\n   <rico:hasAuthor>https://amp.acdh.oeaw.ac.at/" + doc_author_abbr[0].lower() + ".html</rico:hasAuthor>")
+            etree.SubElement(record, etree.QName(nsmap["rico"], "isAssociatedWithDate")).text = doc_date
+        if doc_author_01 != "":
+            # partitioning author name string for surname
+            doc_author_01_abbr = doc_author_01.partition(',')
+            etree.SubElement(record, etree.QName(nsmap["rico"], "hasAuthor")).text = "https://amp.acdh.oeaw.ac.at/" + doc_author_01_abbr[0].lower() + ".html"
+        if doc_author_02 != "":
+            doc_author_02_abbr = doc_author_02.partition(',')
+            etree.SubElement(record, etree.QName(nsmap["rico"], "hasAuthor")).text = "https://amp.acdh.oeaw.ac.at/" + doc_author_02_abbr[0].lower() + ".html"
         if doc_content_type != "":
-            rdf_file.write("\n   <rico:hasContentOfType>" + doc_content_type + "</rico:hasContentOfType>")
+            etree.SubElement(record, etree.QName(nsmap["rico"], "hasContentOfType")).text = doc_content_type
         if doc_language != "":
-            rdf_file.write("\n   <rico:hasOrHadLanguage>" + doc_language + "</rico:hasOrHadLanguage>")
-        rdf_file.write("\n</rico:Record>")
-        rdf_file.write("\n")
+            etree.SubElement(record, etree.QName(nsmap["rico"], "hasOrHadLanguage")).text = doc_language
+        # add to record numerator
         number_records += 1
+
         ''''# TEST TEST TEST !!!!
         with open("C:/Users/tfruehwirth/Desktop/amp-data/data/editions/amp-transcript__" + doc_id + ".xml", "r+") as edition_file:
             contents = edition_file.read().replace("      </fileDesc>", "      </fileDesc>"
@@ -63,39 +69,34 @@ with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.
             edition_file.seek(0)
             edition_file.truncate()
             edition_file.write(contents)
+    
+            # TEST TEST TEST !!!'''
 
-        '''# TEST TEST TEST !!!
-
-# introduce ric-agents section
-rdf_file.write("\n<!-- ric agents -->")
-rdf_file.write("\n")
-
-# open `ric.metadata.csv`
 with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.csv", "r", newline='') as csv_file:
-    # read file and map data in dictionary
     table = csv.DictReader(csv_file, delimiter=',', quotechar='"')
     for row in table:
-        # store cell data in variable
-        doc_agent = row['document_author']
-        # test if variable is part of agents list
-        if doc_agent not in doc_agent_list:
-            # insert variable into ric-agent entry in rdf/xml file
-            doc_agent_abbr = doc_agent.partition(',')
-            rdf_file.write("\n<rico:Agent rdf:about=\"https://amp.acdh.oeaw.ac.at/" + doc_agent_abbr[0].lower() + ".html\">")
-            rdf_file.write("\n   <rico:hasOrHadAgentName>")
-            rdf_file.write("\n      " + doc_agent)
-            rdf_file.write("\n   </rico:hasOrHadAgentName>")
-            rdf_file.write("\n</rico:Agent>")
-            rdf_file.write("\n")
-            # add unlisted agent to agents list
-            doc_agent_list.append(doc_agent)
+        doc_agent_01 = row['document_author_01']
+        doc_agent_02 = row['document_author_02']
+        # test if variable is part of agent list or empty
+        if doc_agent_01 not in doc_agent_list and doc_agent_01 != "":
+            doc_agent_01_abbr = doc_agent_01.partition(',')
+            agent = etree.SubElement(root, etree.QName(nsmap["rico"], "Agent"))
+            agent.attrib[etree.QName(nsmap["rdf"], "about")] = "https://amp.acdh.oeaw.ac.at/" + doc_agent_01_abbr[0].lower() + ".html"
+            etree.SubElement(agent, etree.QName(nsmap["rico"], "hasOrHadAgentName")).text = doc_agent_01
+            # add unlisted agent to agent list
+            doc_agent_list.append(doc_agent_01)
+            # add to agent numerator
+            number_agents += 1
+        if doc_agent_02 not in doc_agent_list and doc_agent_02 != "":
+            doc_agent_02_abbr = doc_agent_02.partition(',')
+            agent = etree.SubElement(root, etree.QName(nsmap["rico"], "Agent"))
+            agent.attrib[etree.QName(nsmap["rdf"], "about")] = "https://amp.acdh.oeaw.ac.at/" + doc_agent_02_abbr[0].lower() + ".html"
+            etree.SubElement(agent, etree.QName(nsmap["rico"], "hasOrHadAgentName")).text = doc_agent_02
+            doc_agent_list.append(doc_agent_02)
             number_agents += 1
 
-# close rdf/xml root element
-rdf_file.write("\n</rdf:RDF>")
-
-# close rdf/xml file
-rdf_file.close()
+with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.rdf", "w") as rdf_file:
+    rdf_file.write(str(etree.tostring(root, encoding="utf-8", xml_declaration=True, pretty_print=True).decode("utf-8")))
 
 # print log
 print("")
