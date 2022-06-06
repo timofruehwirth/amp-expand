45ce4f74db21ec78c796bcc0db83147a0b865eea
timofruehwirth <xml.codemonkey@gmail.com>
2022-06-06 11:00:24 +0200

    add  class and  object for managing CSV cell data

--- ric_metadata/data/py/ric_metadata.py
+++ ric_metadata/data/py/ric_metadata.py
@@ -7,7 +7,7 @@ import csv
 from datetime import datetime
 
 # this script uses the lxml library for creating xml
-# alternatively the rdflib library for creating rdf might be used
+# alternatively use the rdflib library for creating rdf
 
 # import lxml.etree library
 from lxml import etree
@@ -23,82 +23,79 @@ doc_agent_list = []
 number_records = 0
 number_agents = 0
 
+# create a `Document` class
+class Document:
+    def __init__(self, doc_id, doc_title, doc_date, doc_author_01, doc_author_02, doc_author_03, doc_author_04, doc_language, doc_content_type):
+        # assigning values:
+        self.doc_id = doc_id
+        self.doc_title = doc_title
+        self.doc_date = doc_date
+        self.doc_author_01 = doc_author_01
+        self.doc_author_02 = doc_author_02
+        self.doc_author_03 = doc_author_03
+        self.doc_author_04 = doc_author_04
+        self.doc_language = doc_language
+        self.doc_content_type = doc_content_type
+
 # open `ric_metadata.csv`
 with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.csv", "r", newline='') as csv_file:
     # read csv file and map data in dictionary
     table = csv.DictReader(csv_file, delimiter=',', quotechar='"')
     for row in table:
-        # store cell data in variables
-        doc_id = row['document_id']
-        doc_title = row['document_title']
-        doc_date = row['document_date']
-        doc_author_01 = row['document_author_01']
-        doc_author_02 = row['document_author_02']
-        doc_author_03 = row["document_author_03"]
-        doc_author_04 = row["document_author_04"]
-        doc_language = row['document_language']
-        doc_content_type = row['content_type']
+        # store cell data as features of a `document_object` object of the `Document` class
+        document_object = Document(row['document_id'], row['document_title'], row['document_date'],
+                                   row['document_author_01'], row['document_author_02'], row['document_author_03'],
+                                   row['document_author_04'], row['document_language'], row['content_type'])
         # create rdf/xml record sub-elements
         record = etree.SubElement(root, etree.QName(nsmap['rico'], 'Record'))
-        record.attrib[etree.QName(nsmap["rdf"], "about")] = "https://amp.acdh.oeaw.ac.at/amp-transcript__" + doc_id
+        record.attrib[etree.QName(nsmap["rdf"], "about")] = "https://amp.acdh.oeaw.ac.at/amp-transcript__" + document_object.doc_id
         # insert variables unless empty
-        if doc_id != "":
-            etree.SubElement(record, etree.QName(nsmap["rico"], "hasOrHadIdentifier")).text = "amp_" + doc_id
-        if doc_title != "":
-            etree.SubElement(record, etree.QName(nsmap["rico"], "hasOrHadName")).text = doc_title
-        if doc_date != "":
-            etree.SubElement(record, etree.QName(nsmap["rico"], "isAssociatedWithDate")).text = doc_date
-        if doc_author_01 != "":
-            # partitioning author name string for surname and first-name initial
-            doc_author_01_abbr = doc_author_01.partition(',')
+        if document_object.doc_id != "":
+            etree.SubElement(record, etree.QName(nsmap["rico"], "hasOrHadIdentifier")).text = "amp_" + document_object.doc_id
+        if document_object.doc_title != "":
+            etree.SubElement(record, etree.QName(nsmap["rico"], "hasOrHadName")).text = document_object.doc_title
+        if document_object.doc_date != "":
+            etree.SubElement(record, etree.QName(nsmap["rico"], "isAssociatedWithDate")).text = document_object.doc_date
+        if document_object.doc_author_01 != "":
+            # partition author name string for surnames plus first-name initials
+            doc_author_01_abbr = document_object.doc_author_01.partition(',')
             etree.SubElement(record, etree.QName(nsmap["rico"], "hasAuthor")).text = "https://amp.acdh.oeaw.ac.at/" + doc_author_01_abbr[0].lower() + "_" + doc_author_01_abbr[2][1].lower() + ".html"
-        if doc_author_02 != "":
-            doc_author_02_abbr = doc_author_02.partition(',')
+        if document_object.doc_author_02 != "":
+            doc_author_02_abbr = document_object.doc_author_02.partition(',')
             etree.SubElement(record, etree.QName(nsmap["rico"], "hasAuthor")).text = "https://amp.acdh.oeaw.ac.at/" + doc_author_02_abbr[0].lower() + "_" + doc_author_02_abbr[2][1].lower() + ".html"
-        if doc_author_03 != "":
-            doc_author_03_abbr = doc_author_03.partition(',')
+        if document_object.doc_author_03 != "":
+            doc_author_03_abbr = document_object.doc_author_03.partition(',')
             etree.SubElement(record, etree.QName(nsmap["rico"], "hasAuthor")).text = "https://amp.acdh.oeaw.ac.at/" + doc_author_03_abbr[0].lower() + "_" + doc_author_03_abbr[2][1].lower() + ".html"
-        if doc_author_04 != "":
-            doc_author_04_abbr = doc_author_04.partition(',')
+        if document_object.doc_author_04 != "":
+            doc_author_04_abbr = document_object.doc_author_04.partition(',')
             etree.SubElement(record, etree.QName(nsmap["rico"], "hasAuthor")).text = "https://amp.acdh.oeaw.ac.at/" + doc_author_04_abbr[0].lower() + "_" + doc_author_04_abbr[2][1].lower() + ".html"
-        if doc_content_type != "":
-            etree.SubElement(record, etree.QName(nsmap["rico"], "hasContentOfType")).text = doc_content_type
-        if doc_language != "":
-            etree.SubElement(record, etree.QName(nsmap["rico"], "hasOrHadLanguage")).text = doc_language
-        # add to record numerator
+        if document_object.doc_content_type != "":
+            etree.SubElement(record, etree.QName(nsmap["rico"], "hasContentOfType")).text = document_object.doc_content_type
+        if document_object.doc_language != "":
+            etree.SubElement(record, etree.QName(nsmap["rico"], "hasOrHadLanguage")).text = document_object.doc_language
+        # add ric record entry to record numerator
         number_records += 1
-        # with open("C:/Users/tfruehwirth/Desktop/Untitled-1.xml") as test_file:
-        #    test_file.write(str(etree.tostring(root, encoding="utf-8", xml_declaration=True, pretty_print=True).decode("utf-8")))
-
-        ''' TEST TEST TEST
-        with open("C:/Users/tfruehwirth/Desktop/amp-data/data/editions/amp-transcript__" + doc_id + ".xml", "r+") as edition_file:
-            edition_file_string = edition_file.read()
-            replace_start = edition_file_string.find("</fileDesc>")
-            replace_end = edition_file_string.find("<encodingDesc>")
-            replace_substring = edition_file_string[replace_start:replace_end+14]
-            addition = str(etree.tostring(root, encoding="utf-8", xml_declaration=True, pretty_print=True).decode("utf-8"))
-            print(addition)
-            '''
 
 with open("C:/Users/tfruehwirth/Desktop/amp-data/data/ric_metadata/ric_metadata.csv", "r", newline='') as csv_file:
     table = csv.DictReader(csv_file, delimiter=',', quotechar='"')
     for row in table:
+        # store author names in variables, and store variables in `doc_agents` list to enable `for` loop
         doc_agent_01 = row["document_author_01"]
         doc_agent_02 = row["document_author_02"]
         doc_agent_03 = row["document_author_03"]
         doc_agent_04 = row["document_author_04"]
-        doc_agents = [doc_agent_01, doc_agent_02, doc_agent_03, doc_author_04]
+        doc_agents = [doc_agent_01, doc_agent_02, doc_agent_03, doc_agent_04]
         for doc_agent in doc_agents:
-            # test if variable is part of ric agent list or empty
+            # test if variables are part of ric agent list or empty
             if doc_agent not in doc_agent_list and doc_agent != "":
                 # create rdf/xml agent sub-elements
                 agent = etree.SubElement(root, etree.QName(nsmap["rico"], "Agent"))
                 doc_agent_abbr = doc_agent.partition(",")
                 agent.attrib[etree.QName(nsmap["rdf"], "about")] = "https://amp.acdh.oeaw.ac.at/" + doc_agent_abbr[0].lower() + "_" + doc_agent_abbr[2][1].lower() + ".html"
                 etree.SubElement(agent, etree.QName(nsmap["rico"], "hasOrHadAgentName")).text = doc_agent
-                # add unlisted agent to agent list
+                # add unlisted agent to ric agent list
                 doc_agent_list.append(doc_agent)
-                # add ric agent entry generated to agent numerator
+                # add ric agent entry to agent numerator
                 number_agents += 1
 
 # write rdf/xml tree in rdf file
